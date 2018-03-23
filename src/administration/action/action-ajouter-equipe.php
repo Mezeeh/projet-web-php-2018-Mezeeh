4682850b579701e7397808eb16179766b59e8dad
Mezeeh <mezeeh@hotmail.com>
2018-03-23 16:13:14 -0400

    Filtre du logo

--- src/administration/action/action-ajouter-equipe.php
+++ src/administration/action/action-ajouter-equipe.php
@@ -9,8 +9,8 @@ if(!empty($_POST['action-ajouter-equipe']))
         'composition' => FILTER_SANITIZE_STRING, // TODO : Filtrer pour l'image
     );
     $equipe = filter_var_array($_POST, $filtreEquipe);
-    print_r($_FILES["logo"]["name"]);
-    $equipe['logo'] = $_FILES["logo"]["name"];
+    //print_r($_FILES["logo"]["name"]);
+    $equipe['logo'] = filter_var($_FILES["logo"]["name"], FILTER_SANITIZE_STRING);
     
     include_once "../accesseur/EquipeDAO.php";
     $equipeDAO = new EquipeDAO();
