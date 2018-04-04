5a7217614a3734287b53c2b837e455cdefd69e18
Mezeeh <mezeeh@hotmail.com>
2018-04-04 16:46:39 -0400

    Requete DAO ajouter prepare

--- src/accesseur/MembreDAO.php
+++ src/accesseur/MembreDAO.php
@@ -17,17 +17,16 @@
         function ajouterMembre($membre){
             global $pdo;
 
-            //$AJOUTER_MEMBRE = "INSERT into membre(prenom, nom, pseudonyme, motdepasse, courriel, /* genre */) VALUES(:prenom, :nom, :pseudonyme, :motdepasse, :courriel, /* :genre */)";
-            $AJOUTER_MEMBRE = "INSERT into membre(prenom, nom, pseudonyme, motdepasse, courriel, genre) 
-            VALUES('". $membre['prenom'] ."', '". $membre['nom'] ."', '". $membre['pseudonyme'] ."', '". $membre['motdepasse'] ."', '". $membre['courriel'] ."', '". $membre['genre'] ."')";
-            $requeteAjouterMembre = $pdo->prepare($AJOUTER_MEMBRE);
+            $AJOUTER_MEMBRE = "INSERT into membre(prenom, nom, pseudonyme, motdepasse, courriel, genre) VALUES(:prenom, :nom, :pseudonyme, :motdepasse, :courriel, :genre)";
+            
+            $requeteAjouterMembre = $pdo->prepare($AJOUTER_MEMBRE); 
 
-            /* $requeteAjouterMembre->bindParam(":prenom", $membre['prenom'], PDO::PARAM_STR);
+            $requeteAjouterMembre->bindParam(":prenom", $membre['prenom'], PDO::PARAM_STR);
             $requeteAjouterMembre->bindParam(":nom", $membre['nom'], PDO::PARAM_STR);
             $requeteAjouterMembre->bindParam(":pseudonyme", $membre['pseudonyme'], PDO::PARAM_STR);
             $requeteAjouterMembre->bindParam(":motdepasse", $membre['motdepasse'], PDO::PARAM_STR);
-            $requeteAjouterMembre->bindParam(":courriel", $membre['courriel'], PDO::PARAM_STR); */
-            //$requeteAjouterMembre->bindParam(":genre", $genre);
+            $requeteAjouterMembre->bindParam(":courriel", $membre['courriel'], PDO::PARAM_STR);
+            $requeteAjouterMembre->bindParam(":genre", $membre['genre'], PDO::PARAM_INT);
             $requeteAjouterMembre->execute();
             
             //print_r($requeteAjouterMembre);
