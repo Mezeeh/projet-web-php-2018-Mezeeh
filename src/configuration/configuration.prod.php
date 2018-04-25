a9c222d3f13b72ef2a7474108548d18069c25144
Mezeeh <1643216@cgmatane.qc.ca>
2018-04-25 06:55:51 -0400

    Config dev et prod

---
+++ src/configuration/configuration.prod.php
@@ -0,0 +1,24 @@
+<?php
+	// Config pour l'upload d'une image
+	$site = new stdClass();
+	
+	$site->chemin = new stdClass();
+	//$site->chemin->racine = $_SERVER['DOCUMENT_ROOT'] . '/eSportHQ/src/';
+	$site->chemin->racine = '/var/www/eSportHQ/';
+	//$site->chemin->illustration = $site->chemin->racine . 'illustration/';
+	$site->chemin->illustration = $site->chemin->racine . 'illustration\\';
+	//$site->chemin->miniature = $site->chemin->racine . 'illustration/miniature/';
+	$site->chemin->miniature = $site->chemin->racine . 'illustration\miniature\\';
+	
+	$site->url = new stdClass();
+	$site->url->racine = 'http://149.56.13.19/eSportHQ/';
+	$site->url->illustration = 'http://149.56.13.19/eSportHQ/illustration/';
+	$site->url->miniature = 'http://149.56.13.19/eSportHQ/src/illustration/mini/';
+	
+	// Config pour la traduction
+	$traduction = new stdClass();
+	$traduction->chemin = $site->chemin->racine . 'locale/';
+	$traduction->domaine = 'messages';
+	$traduction->locale = 'en_CA.utf8'; // $locale = "en_CA";
+	$traduction->locales = ['fr_CA.utf8','en_CA.utf8'];
+?>
