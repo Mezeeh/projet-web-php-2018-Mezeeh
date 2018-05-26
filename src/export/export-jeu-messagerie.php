5a9827c2f3215cf4abf4df8064919843246e89e6
Mezeeh <1643216@cgmatane.qc.ca>
2018-05-26 16:05:03 -0400

    Fix script messagerie avec changement de librairie

--- src/export/export-jeu-messagerie.php
+++ src/export/export-jeu-messagerie.php
@@ -3,24 +3,15 @@
 	$infos = filter_input_array(INPUT_POST, FILTER_SANITIZE_STRING);
 	//print_r($infos);
 	
-	use PHPMailer\PHPMailer\PHPMailer;
-	use PHPMailer\PHPMailer\Exception;
+	require '../lib/phpmailer/PHPMailerAutoload.php';
+	$message = new PHPMailer;
 
-	require '../lib/phpmailer/src/Exception.php';
-	require '../lib/phpmailer/src/PHPMailer.php';
-	
-	$message = new PHPMailer();
-
-	$message->setFrom($infos['courriel']);
-	$message->addAddress('micturcotte97@gmail.com', $infos['nom'] . ', ' . $infos['prenom']);
+	$message->setFrom($infos['courriel'], $infos['prenom'] . ' ' . $infos['nom']);
+	$message->addAddress('mezeeh@hotmail.com');
 	$message->addAddress($infos['courriel']);               
 	//$message->addReplyTo('info@ici.com', 'Information');
 	$message->Subject = $infos['sujet'];
 	$message->Body = $infos['message'];
 
-	//print_r($message);
-	if(!$message->send()) 
-		echo 'Erreur de messagerie: ' . $message->ErrorInfo;
-	else
-		echo 'Message envoyé';
+	echo $message->send() ? 'Message envoyé' : 'Erreur de messagerie: ' . $message->ErrorInfo;
 ?>
