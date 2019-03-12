e7b07c09170cdb5f2f279288395f87da69e87f19
Unknown <1643216@cgmatane.qc.ca>
2019-03-12 10:03:20 -0400

    Intégration de l'AJax dans l'index pour afficher les tournois à venir Closes #2

--- src/index.php
+++ src/index.php
@@ -29,6 +29,11 @@
 		$heureRestantAvantDota2TI = 0;
 		$jourRestantAvantDota2TI = 0;
 	}
+
+	include_once "accesseur/JeuDAO.php";
+
+	$jeuDAO = new JeuDAO();
+	$listeCompetitions = $jeuDAO->rechercherCompetition();
 ?>
 
 <!doctype html>
@@ -50,15 +55,81 @@
 	</ul>
     
     <section id="contenu">
-        <header><h2>Accueil</h2></header>
+        <header>
+			<h2>Accueil</h2>
+				
+			<script type="text/javascript" src="lib/Ajax.js"></script>
+        	<script type="text/javascript">
+				var competitionChoisie;
+				function afficherTempsAvantCompetition(competition){
+					if(!competition){
+						competition = document.querySelector('#competitions');
+						competition = competition.options[competition.selectedIndex].value;
+					}
+
+					parametre = "competition=" + competition;
+					competitionChoisie = competition;
+    				// console.log(parametre);
+
+					ajax = new Ajax();
+					url = 'http://localhost/eSportHQ/src/action/action-temps-restant-avant-competition.php';
+					// url = 'http://localhost/esporthq/action/action-temps-restant-avant-competition.php';
+					ajax.executer("GET", url, parametre, recevoirLeTempsRestant);
+				}
+
+				function recevoirLeTempsRestant(ajax){
+					// console.log("recevoirLeTempsRestant");
+					reponse = JSON.parse(ajax.responseText);
+					// console.log('temps restant ='+reponse);
+
+					joursRestants = reponse.dateProchainTournoi.joursRestants;
+					heuresRestantes = reponse.dateProchainTournoi.heuresRestantes;
+					minutesRestantes = reponse.dateProchainTournoi.minutesRestantes;
+					secondesRestantes = reponse.dateProchainTournoi.secondesRestantes;
+
+					mettreAJourTempsRestant(joursRestants, heuresRestantes, minutesRestantes, secondesRestantes);
+				}
+
+				function mettreAJourTempsRestant(joursRestants, heuresRestantes, minutesRestantes, secondesRestantes){
+					//console.log("mettreAJourTempsRestant");
+
+					document.querySelector('#compteur-temps-avant-competition').style.display = "inline-block";
+					
+					document.querySelector('#compteur-temps-avant-competition').innerHTML = `Il reste ${joursRestants} jours, ${heuresRestantes} heures, ${minutesRestantes} minutes et ${secondesRestantes} secondes avant ${competitionChoisie}.`;
+
+					setTimeout(() => {
+						afficherTempsAvantCompetition();
+					}, 1000);
+				}
+			</script>
+
+			<style type="text/css">
+				#compteur-temps-avant-competition{
+					display: none;
+				}
+			</style>
+		</header>
         
         <div>
             <p>Nous somme le <?=$dateDuJour?></p>
         </div>
 
 		<div>
-			<p><strong><?=$jourRestantAvantDota2TI?> jour(s) <?=$heureRestantAvantDota2TI?> heure(s) <?=$minuteRestantAvantDota2TI?> minute(s) et <?=$secondeRestantAvantDota2TI?> seconde(s)</strong> avant L'international de DOTA 2</p>
+			Tournoi à venir:
+			<select name="competitions" id="competitions" onchange="afficherTempsAvantCompetition(this.value)">
+				<option selected disabled hidden>Choisir une compétition</option>
+
+				<?php
+					foreach ($listeCompetitions as $prochainTournoi) {
+						?>
+							<option value="<?= $prochainTournoi["prochainTournoi"]; ?>"><?= $prochainTournoi["prochainTournoi"]; ?></option>
+						<?php
+					}
+				?>
+			</select>
 		</div>
+
+		<div id="compteur-temps-avant-competition"></div>
     </section>
 </body>
 </html>
