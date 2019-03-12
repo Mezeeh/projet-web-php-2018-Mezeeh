9d4f459cf0a5de5c7bfe6b1fd408ad3d5ff1779c
Unknown <1643216@cgmatane.qc.ca>
2019-03-12 08:03:35 -0400

    Fonctions DAO pour les modifications du membre #1

--- src/accesseur/MembreDAO.php
+++ src/accesseur/MembreDAO.php
@@ -31,5 +31,35 @@
             
             //print_r($requeteAjouterMembre);
         }
+
+        function modifierIdentiteMembre($membre){
+            global $pdo;
+
+            $MODIFIER_MEMBRE = "UPDATE membre SET prenom = :prenom, nom = :nom, courriel = :courriel WHERE pseudonyme = :pseudonyme";
+            
+            $requeteModifierMembre = $pdo->prepare($MODIFIER_MEMBRE); 
+
+            $requeteModifierMembre->bindParam(":prenom", $membre['prenom'], PDO::PARAM_STR);
+            $requeteModifierMembre->bindParam(":nom", $membre['nom'], PDO::PARAM_STR);
+            $requeteModifierMembre->bindParam(":courriel", $membre['courriel'], PDO::PARAM_STR);
+            $requeteModifierMembre->bindParam(":pseudonyme", $membre['pseudonyme'], PDO::PARAM_STR);
+            $requeteModifierMembre->execute();
+            
+            // $requeteModifierMembre->debugDumpParams();
+        }
+
+        function modifierSecuriteMembre($membre){
+            global $pdo;
+
+            $MODIFIER_MEMBRE = "UPDATE membre SET motdepasse = :motdepasse WHERE pseudonyme = :pseudonyme";
+            
+            $requeteModifierMembre = $pdo->prepare($MODIFIER_MEMBRE); 
+
+            $requeteModifierMembre->bindParam(":motdepasse", $membre['motdepasse'], PDO::PARAM_STR);
+            $requeteModifierMembre->bindParam(":pseudonyme", $membre['pseudonyme'], PDO::PARAM_STR);
+            $requeteModifierMembre->execute();
+            
+            // $requeteModifierMembre->debugDumpParams();
+        }
     }
 ?>
