e9c21ab66cee5cb410f1deffb55f314d827d8d64
Unknown <1643216@cgmatane.qc.ca>
2019-03-12 10:02:36 -0400

    Script de calcul de temps restant avec retour en JSON #2

---
+++ src/action/action-temps-restant-avant-competition.php
@@ -0,0 +1,52 @@
+<?php
+    include "../accesseur/JeuDAO.php";
+    // print_r($_GET);
+
+    date_default_timezone_set('Canada/Eastern');
+
+    $competition = filter_var($_GET['competition'], FILTER_SANITIZE_STRING);
+
+	$jeuDAO = new JeuDAO();
+	$dateProchainTournoi = $jeuDAO->rechercherDateCompetition($competition)[0];
+    // print_r($dateProchainTournoi);
+    $dateProchainTournoi = strtotime($dateProchainTournoi);
+    $date = strtotime(date("Y-m-d h:i:s", time()));
+
+    $tempsRestant = $dateProchainTournoi - $date;
+    //print_r($tempsRestant);
+
+    if($tempsRestant > 0){
+        $minuteRestant = $tempsRestant / 60;
+        $heureRestant = $minuteRestant / 60;
+        $jourRestant = $heureRestant / 24;
+        
+        $secondeRestant = floor($tempsRestant % 60);
+        $minuteRestant = floor($minuteRestant % 60);
+        $heureRestant = floor($heureRestant % 24);
+        $jourRestant = floor($jourRestant); 
+        
+        /* print_r($jourRestant + " ");
+        print_r(" ");
+        print_r($heureRestant + " ");
+        print_r(" ");
+        print_r($minuteRestant + " ");
+        print_r(" ");
+        print_r($secondeRestant + " ");
+        print_r(" "); */
+    } else {
+        $secondeRestant = 0;
+        $minuteRestant = 0;
+        $heureRestant = 0;
+        $jourRestant = 0; 
+    } 
+    
+    header("Content-type: text/json");
+?>
+{
+    "dateProchainTournoi": {
+                                "joursRestants": <?=$jourRestant?>,
+                                "heuresRestantes": <?=$heureRestant?>,
+                                "minutesRestantes": <?=$minuteRestant?>,
+                                "secondesRestantes": <?=$secondeRestant?>
+                            }
+}
